cd94cdccc4719411975b3a2f3d30ba2b0e1d6ff2
Micle Bu <mekery@qq.com>
2021-12-12 14:35:54 +0800

    feat: 🎸 Loot

--- ERC721/FullFeaturedERC721.sol
+++ ERC721/FullFeaturedERC721.sol
@@ -1,56 +1,68 @@
-// contracts/ERC721/ERC721Token.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 /**
- * @dev Basic ERC721 Token
+ * @dev Full Featured ERC721 Token
  * 
- * Customized {minter}, {name}, {symbol} of the Token.
+ * Customized {name}, {symbol}, {pauser}, {minter} of the Token.
  *
  * hhttps://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol
  */
-
-contract ERC721Token is ERC721, ERC721Enumerable, ERC721URIStorage, AccessControl {
+contract FullFeaturedERC721 is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, AccessControl, ERC721Burnable {
     using Counters for Counters.Counter;
 
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
     constructor(
-        address minter_,
         string memory name_,
-        string memory symbol_
+        string memory symbol_,
+        address pauser_,
+        address minter_
+
     ) ERC721(name_, symbol_) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, pauser_);
         _grantRole(MINTER_ROLE, minter_);
     }
 
-    function safeMint(address to, string memory uri) 
-        public onlyRole(MINTER_ROLE)
-        returns (uint256)
-     {
+    // function _baseURI() internal pure override returns (string memory) {
+    //     return "https://yitoken.im";
+    // }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function safeMint(address to, string memory uri) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
-
-        return tokenId;
     }
-
-    // The following functions are overrides required by Solidity.
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
+        whenNotPaused
         override(ERC721, ERC721Enumerable)
     {
         super._beforeTokenTransfer(from, to, tokenId);
     }
+
+    // The following functions are overrides required by Solidity.
 
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
